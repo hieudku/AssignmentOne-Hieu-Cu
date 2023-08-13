96fcdb96ae65d178122ae1e7649b3a6510f2e821
Huba <hieu.dcu@gmail.com>
2023-08-14 10:46:40 +1200

    initial commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace AssignmentOne_Hieu_Cu
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the number of sales record");
+            int recordNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the following: ");
+            for (int i = 0; i < recordNumber; i++)
+            {
+                Console.WriteLine("Product ID: ");
+                string productId = Console.ReadLine();
+                string[] IdArr = new string[recordNumber];
+
+
+                Console.WriteLine("Quantity sold: ");
+                int soldQuant = int.Parse(Console.ReadLine());
+                int[] soldArr = new int[recordNumber];
+
+
+                Console.WriteLine("Sales amount: ");
+                double salesAmount = Convert.ToDouble(Console.ReadLine());
+                double[] salesArr = new double[recordNumber];
+            }
+        }
+
+    }
+    static class GetRecords
+    {
+        static void Record(string productId, int soldQuant, double salesAmount)
+        {
+            
         }
     }
 }
