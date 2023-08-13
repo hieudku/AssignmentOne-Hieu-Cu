394d62759c8326bba23c58e27d6cc42a558167e7
Huba <hieu.dcu@gmail.com>
2023-08-14 11:02:31 +1200

    Get records and total amount

--- Program.cs
+++ Program.cs
@@ -10,34 +10,49 @@ namespace AssignmentOne_Hieu_Cu
     {
         static void Main(string[] args)
         {
+            int recordNumber;
+            double soldQuant;
+            double salesAmount;
+            string[] IdArr;
+            double[] soldArr;
+            double[] salesArr;
             Console.WriteLine("Please enter the number of sales record");
-            int recordNumber = int.Parse(Console.ReadLine());
+            recordNumber = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter the following: ");
-            for (int i = 0; i < recordNumber; i++)
+            for (int i = 0; i <= recordNumber; i++)
             {
                 Console.WriteLine("Product ID: ");
                 string productId = Console.ReadLine();
-                string[] IdArr = new string[recordNumber];
-
+                IdArr = new string[recordNumber];
+                for (int x = 0; x < IdArr.Length; x++)
+                {
+                    IdArr[x] = productId;
+                }
 
                 Console.WriteLine("Quantity sold: ");
-                int soldQuant = int.Parse(Console.ReadLine());
-                int[] soldArr = new int[recordNumber];
-
+                soldQuant = int.Parse(Console.ReadLine());
+                soldArr = new double[recordNumber];
+                for (int y = 0; y < IdArr.Length; y++)
+                {
+                    soldArr[y] = soldQuant;
+                }
 
                 Console.WriteLine("Sales amount: ");
-                double salesAmount = Convert.ToDouble(Console.ReadLine());
-                double[] salesArr = new double[recordNumber];
+                salesAmount = Convert.ToDouble(Console.ReadLine());
+                salesArr = new double[recordNumber];
+                for (int z = 0; z < IdArr.Length; z++)
+                {
+                    salesArr[z] = salesAmount;
+                }
+                GetTotal(soldQuant, salesAmount);
             }
         }
-
-    }
-    static class GetRecords
-    {
-        static void Record(string productId, int soldQuant, double salesAmount)
+        static double GetTotal(double sold, double amount)
         {
-            
+            double total = sold * amount;
+            Console.WriteLine($"Total: {total}");
+            return total;
         }
     }
 }
