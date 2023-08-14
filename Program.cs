76968e70eb8fa9766c67df7b207bd8e230ca94c1
Huba <hieu.dcu@gmail.com>
2023-08-14 13:52:29 +1200

    Added comments.

--- Program.cs
+++ Program.cs
@@ -10,53 +10,65 @@ namespace AssignmentOne_Hieu_Cu
     {
         public static void Main(string[] args)
         {
-            int recordNumber;
-            double soldQuant;
-            double salesAmount;
-            double totalSales;
-            string[] IdArr;
-            double[] soldArr;
-            double[] salesArr;
-            Console.WriteLine("Please enter the number of sales record");
-            recordNumber = int.Parse(Console.ReadLine());
+            Record sale = new Record(); // Create object instance of Record class.
+            sale.GetRecord(); // Call method from Record class.
+        }
+    }
+    class Record // parent class
+    {
+        public int recordsNumber;
+        public string prodId;
+        public double quantSold;
+        public double salesAmount;
+        public string[] productArr;
+        public double[] quantArr;
+        public double[] amountArr;
+        public void GetRecord()
+        {
+            Console.WriteLine("Enter the number of records: ");
+            recordsNumber = int.Parse(Console.ReadLine());
+            productArr = new string[recordsNumber];
+            quantArr = new double[recordsNumber];
+            amountArr = new double[recordsNumber];
 
-            Console.WriteLine("Please enter the following: ");
-            for (int i = 0; i <= recordNumber; i++) // Determine the number of records.
-            {
+            for (int i = 0; i < recordsNumber; i++) { // For loop to ask for values according to number of records.
                 Console.WriteLine("Product ID: ");
-                string productId = Console.ReadLine();
-                IdArr = new string[recordNumber];
-                for (int x = 0; x < IdArr.Length; x++) // Iterate & assign each array index as each input value.
-                {
-                    IdArr[x] = productId;
-
-                }
+                prodId = Console.ReadLine();
+                    productArr[i] = prodId;
 
                 Console.WriteLine("Quantity sold: ");
-                soldQuant = int.Parse(Console.ReadLine());
-                soldArr = new double[recordNumber];
-                for (int y = 0; y < IdArr.Length; y++) // Iterate & assign each array index as each input value.
-                {
-                    soldArr[y] = soldQuant;
-
-                }
+                quantSold = Convert.ToDouble(Console.ReadLine());
+                    quantArr[i] = quantSold ;
 
                 Console.WriteLine("Sales amount: ");
                 salesAmount = Convert.ToDouble(Console.ReadLine());
-                salesArr = new double[recordNumber];
-                for (int z = 0; z < IdArr.Length; z++) // Iterate & assign each array index as each input value.
-                {
-                    salesArr[z] = salesAmount;
-                }
-                totalSales = Multiply(soldQuant, salesAmount);
-                Console.WriteLine($"Total Sales for product #{productId} is {totalSales}");
-            }// End of recordNumber for loop.
+                    amountArr[i] = salesAmount;
+            }
+            for (int i = 0; i < recordsNumber; i++)
+            {
+                Console.WriteLine(amountArr[i]);
+            }
 
-        }// End of Main
-        static double Multiply(double sold, double amount)
+            GetTotalSales arr = new GetTotalSales(); // Create object instance of GetArray class.
+            double[] totalAmount = arr.GetTotalArr(quantArr, amountArr); // Assign new array to store all total sales amount by calling array method which calculate them.
+                                                                           
+
+            for (int t = 0; t < totalAmount.Length; t++)
+            {
+                Console.WriteLine(totalAmount[t]);
+            }
+        }
+    }
+    class GetTotalSales : Record // Child class of Record. Parse array to GetTotalArr method.
+    {
+        public double[] GetTotalArr(double[] quantArr, double[] amountArr)
         {
-            double total = sold * amount;
-            return total;
+            double[] totalAmount = new double[amountArr.Length];
+            for (int i = 0; i < amountArr.Length; i++)
+            {
+                totalAmount[i] = quantArr[i] * amountArr[i]; // Iterate through amountArr array to work out each total amount (quantity * amount) of the products.
+            }
+            return totalAmount; // Return totalAmount back to where it was called (Record class).
         }
     }
 }
