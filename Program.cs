e6739af23c96672c0c480f8380148b61274f9b56
Huba <hieu.dcu@gmail.com>
2023-08-14 15:07:18 +1200

    Added more comments.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace AssignmentOne_Hieu_Cu
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("**********Welcome to Sales Analysis Application**********");
+
             Record sale = new Record(); // Create object instance of Record class.
             sale.GetRecord(); // Call method from Record class.
+
+            Console.WriteLine("*************************END*****************************");
         }
     }
     class Record // parent class
@@ -21,6 +25,7 @@ namespace AssignmentOne_Hieu_Cu
         public double quantSold;
         public double salesAmount;
         public double topProd;
+        public int highestProd;
         public double[] productArr;
         public double[] quantArr;
         public double[] amountArr;
@@ -35,34 +40,35 @@ namespace AssignmentOne_Hieu_Cu
 
             for (int i = 0; i < recordsNumber; i++)
             { // For loop to ask for values according to number of records.
-                Console.WriteLine("Product ID: ");
+                Console.WriteLine("Enter Product ID: ");
                 prodId = Convert.ToDouble(Console.ReadLine());
                 productArr[i] = prodId; // As array index increment, assign the next input to that index.
 
-                Console.WriteLine("Quantity sold: ");
+                Console.WriteLine("Enter Quantity sold: ");
                 quantSold = Convert.ToDouble(Console.ReadLine());
                 quantArr[i] = quantSold;
 
-                Console.WriteLine("Sales amount: ");
+                Console.WriteLine("Enter Sales amount: ");
                 salesAmount = Convert.ToDouble(Console.ReadLine());
                 amountArr[i] = salesAmount;
             }
-            for (int i = 0; i < recordsNumber; i++)
-            {
-                Console.WriteLine(amountArr[i]);
-            }
-
-            GetTotalSales total = new GetTotalSales(); // Create object instance of GetArray class.
+            /* Class object creation and Method calls */
+            GetTotalSales total = new GetTotalSales(); // Create object instance of GetTotalSales class.
             totalAmountArr = total.GetTotalArr(quantArr, amountArr); // Assign new array to store all total sales amount by calling array method that has quantity and amount as parameters, which then parsed onto the method to calculate.
 
-
+            Console.WriteLine("Total amount in sales: ");
             for (int t = 0; t < totalAmountArr.Length; t++)
             {
-                Console.WriteLine(totalAmountArr[t]);
+                Console.WriteLine($"Product ID # {productArr[t]} -- {totalAmountArr[t]}  ");
             }
+
+            GetHighestSale highest = new GetHighestSale();
+            highestProd = highest.HighestSales(totalAmountArr);
+            Console.WriteLine($"Product ID with the highest sales: {productArr[highestProd]}");
+
         }
-    }
-    class GetTotalSales : Record // Child class of Record. Parse array to GetTotalArr method.
+    }//End of Record Class.
+    class GetTotalSales // Child class of Record. Parse array to GetTotalArr method.
     {
         public double[] GetTotalArr(double[] quantArr, double[] amountArr)
         {
@@ -73,5 +79,28 @@ namespace AssignmentOne_Hieu_Cu
             }
             return totalAmount; // Return totalAmount back to where it was called (Record class).
         }
-    }
+    }// End of GetTotalSales Class.
+    class GetHighestSale
+    {
+        public int HighestSales(double[] totalAmountArr) // .
+        {
+            double highestSale = 0;
+            int productId = 0; //Assign to 0 as starting point.
+            bool foundHighest = false;
+            for (int i = 0; i < totalAmountArr.Length; i++) // Loop to find the product ID with highest total sales by iterating through the total sales array
+            {
+                if (totalAmountArr[i] > highestSale) // Compare the current highest sales vs each index of the total sales array.
+                {
+                    highestSale = totalAmountArr[i]; //update the current highest sales every round.
+                    productId = i; // Also update the ID to the current value of i every round.
+                    foundHighest = true;
+                }
+            }
+            if (foundHighest) // Once loop finished, and foundHighest is true, execute to return productId.
+            {
+                return productId;
+            }
+            return productId;
+        }
+    }// End of GetHighestSale Class.
 }
