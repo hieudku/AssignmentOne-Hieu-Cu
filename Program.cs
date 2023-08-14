93a49ad2fc50ff98050b5df79d9d6b40f2b5aeba
Huba <hieu.dcu@gmail.com>
2023-08-15 10:26:10 +1200

    Edited and added some comments. Removed public for declared variable (redundant).

--- Program.cs
+++ Program.cs
@@ -12,39 +12,38 @@ namespace AssignmentOne_Hieu_Cu
         {
             Console.WriteLine("**********Welcome to Sales Analysis Application**********");
 
-            Record sale = new Record(); // Create object instance of Record class.
-            sale.GetRecord(); // Call method from Record class.
+            Record sale = new Record();                             // Construct object instances of Record class.
+            sale.GetRecord();                                       // Call method from Record class.
 
             Console.WriteLine("*************************END*****************************");
         }
     }
     class Record // parent class
     {
-        public int recordsNumber;
-        public double prodId;
-        public double quantSold;
-        public double salesAmount;
-        public double topProd;
-        public int highestProd;
-        public double average;
-        public double[] productArr;
-        public double[] quantArr;
-        public double[] amountArr;
-        public double[] totalAmountArr;
+        int recordsNumber;
+        double prodId;
+        double quantSold;
+        double salesAmount;
+        int highestProd;
+        double average;
+        double[] productArr;
+        double[] quantArr;
+        double[] amountArr;
+        double[] totalAmountArr;
         public void GetRecord()
         {
             Console.WriteLine("Enter the number of records: ");
-            recordsNumber = int.Parse(Console.ReadLine());
-            productArr = new double[recordsNumber];
+            recordsNumber = int.Parse(Console.ReadLine());          // Number of records stored in recordsNumber
+            productArr = new double[recordsNumber];                 // Create array with size of recordsNumber value entered.
             quantArr = new double[recordsNumber];
             amountArr = new double[recordsNumber];
 
             for (int i = 0; i < recordsNumber; i++)
-            { // For loop to ask for values according to number of records.
+            {                                                       // For loop to ask for values according to number of records.
                 Console.WriteLine("Enter Product ID: ");
                 prodId = Convert.ToDouble(Console.ReadLine());
 
-                productArr[i] = prodId; // As array index increment, assign the next input to that index.
+                productArr[i] = prodId;                             // As array index increment, assign the next input to that index.
 
                 Console.WriteLine("Enter Quantity sold: ");
                 quantSold = Convert.ToDouble(Console.ReadLine());
@@ -54,64 +53,63 @@ namespace AssignmentOne_Hieu_Cu
                 salesAmount = Convert.ToDouble(Console.ReadLine());
                 amountArr[i] = salesAmount;
             }
-            /* Class object creation and Method calls */
-            GetTotalSales total = new GetTotalSales(); // Create object instance of GetTotalSales class.
-            totalAmountArr = total.GetTotalArr(quantArr, amountArr); // Assign new array to store all total sales amount by calling array method that has quantity and amount as parameters, which then parsed onto the method to calculate.
+                                                                    /* Class objects constructors and Method calls */
+            GetTotalSales total = new GetTotalSales();              // Construct object instance of GetTotalSales class.
+            totalAmountArr = total.GetTotalArr(quantArr, amountArr);// Assign totalAmountArr to store all total sales amount by calling array method that has quantity and amount as parameters, which then parsed onto the method to calculate.
 
             Console.WriteLine("Total amount in sales: ");
-            for (int t = 0; t < totalAmountArr.Length; t++) // Loop to write each element of the totalAmountArr as total sales. 
+            for (int t = 0; t < totalAmountArr.Length; t++)         // Loop to write each element of the totalAmountArr as total sales. 
             {
                 Console.WriteLine($"Product ID # {productArr[t]} -- {totalAmountArr[t]}  ");
             }
 
-            GetHighestSale highest = new GetHighestSale();
+            GetHighestSale highest = new GetHighestSale();          // Construct object instance of GetHighestlSales class.
             highestProd = highest.HighestSales(totalAmountArr);
             Console.WriteLine($"Product ID with the highest sales: {productArr[highestProd]}");
 
-
-            GetAverage aver = new GetAverage();
+            GetAverage aver = new GetAverage();                     // Construct object instance of GetAverage class.
             average = aver.Average(totalAmountArr);
             Console.WriteLine($"The average sales amount of all products: {average}");
         }
-    }//End of Record Class.
-    class GetTotalSales // Child class of Record. Parse array to GetTotalArr method.
+    }                                                               //End of Record Class.
+    class GetTotalSales : Record                                    // Child class of Record. 
     {
-        public double[] GetTotalArr(double[] quantArr, double[] amountArr)
+        public double[] GetTotalArr(double[] quantArr, double[] amountArr) // Parse 2 arrays as parameter to store quantity and amount array.
         {
             double[] totalAmount = new double[amountArr.Length];
             for (int i = 0; i < amountArr.Length; i++)
             {
-                totalAmount[i] = quantArr[i] * amountArr[i]; // Iterate through amountArr array to work out each total amount (quantity * amount) of the products according to their index.
+                totalAmount[i] = quantArr[i] * amountArr[i];        // Iterate through amountArr array to work out each total amount (quantity * amount) of the products according to their index.
             }
-            return totalAmount; // Return totalAmount back to where it was called (Record class).
+            return totalAmount;                                     // Return totalAmount back to where it was called (Record class).
         }
-    }// End of GetTotalSales Class.
-    class GetHighestSale
+    }                                                               // End of GetTotalSales Class.
+    class GetHighestSale : Record                                   // Child class of Record.
     {
-        public int HighestSales(double[] totalAmountArr) // .
+        public int HighestSales(double[] totalAmountArr) 
         {
             double highestSale = 0;
-            int productId = 0; //Assign to 0 as starting point.
+            int productId = 0;                                      //Assign to 0 as starting point.
             bool foundHighest = false;
-            for (int i = 0; i < totalAmountArr.Length; i++) // Loop to find the product ID with highest total sales by iterating through the total sales array
+            for (int i = 0; i < totalAmountArr.Length; i++)         // Loop to find the product ID with highest total sales by iterating through the total sales array
             {
-                if (totalAmountArr[i] > highestSale) // Compare the current highest sales vs each index of the total sales array.
+                if (totalAmountArr[i] > highestSale)                // Compare the current highest sales vs each index of the total sales array.
                 {
-                    highestSale = totalAmountArr[i]; //update the current highest sales every round.
-                    productId = i; // Also update the ID to the current value of i every round.
+                    highestSale = totalAmountArr[i];                //update the current highest sales every round.
+                    productId = i;                                  // Also update the ID to the current value of i every round.
                     foundHighest = true;
                 }
             }
-            if (foundHighest) // Once loop finished, and foundHighest is true, execute to return productId.
+            if (foundHighest)                                       // Once loop finished, and foundHighest is true, execute to return productId.
             {
                 return productId;
             }
             return productId;
         }
-    }// End of GetHighestSale Class.
-    class GetAverage
+    }                                                               // End of GetHighestSale Class.
+    class GetAverage : Record                                       // Child class of Record.
     {
-        public double Average(double[] totalAmountArr)
+        public double Average(double[] totalAmountArr)              // New array as parameter to store the total amount array parsed into the method.
         {
             double average;
             double sum = 0;
